fe175c22edcb32192c87b64402bacb4dc15e69f3
HY <09.hitesh.22b1531059@abes.ac.in>
2024-10-10 15:43:47 +0530

    html

---
+++ basicmain.js
@@ -0,0 +1,5 @@
+import {add,sub} from "./basicjavascrpt.mjs";
+import * as jadoo from "./seconbasic.js";
+
+export {add,sub};
+
---
+++ seconbasic.js
@@ -0,0 +1,3 @@
+const check=require("./basicjavascrpt.mjs");
+console.log("Add",check.add(5,3));
+console.log("Add",check.sub(5,3));
